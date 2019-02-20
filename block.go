468f05ee5d1ae1f1ea5126c95686a84c2f26f083
Danni <38550500+isdanni@users.noreply.github.com>
2019-02-20 13:41:02 +0800

    Create block.go

---
+++ block.go
@@ -0,0 +1,38 @@
+package main
+
+import {
+    "fmt"
+    "net"
+    "os"
+    "path/filepath"
+    "runtime"
+	  "runtime/debug"
+    "runtime/pprof"
+}
+
+const {
+    prefixBlock = "blocks"
+}
+
+var {
+    cfg *config
+}
+
+func blocMain() error {
+
+	interrupt := interruptListener()
+	defer btcdLog.Info("Shutdown complete")
+
+	btcdLog.Infof("Version %s", version())
+
+	if cfg.Profile != "" {
+		go func() {
+			listenAddr := net.JoinHostPort("", cfg.Profile)
+			btcdLog.Infof("Profile server listening on %s", listenAddr)
+			profileRedirect := http.RedirectHandler("/debug/pprof",
+				http.StatusSeeOther)
+			http.Handle("/", profileRedirect)
+			btcdLog.Errorf("%v", http.ListenAndServe(listenAddr, nil))
+		}()
+	}
+ }
